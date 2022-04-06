2d1bb32b8e206cf4027fcd662bc3707670312c9c
Roopika Srinivas <13376391+roopikasrinivas@users.noreply.github.com>
2022-04-06 20:59:05 +0530

    Model Changes

--- src/main/java/com/bits/scalableservices/course/service/entity/Course.java
+++ src/main/java/com/bits/scalableservices/course/service/entity/Course.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -18,7 +15,11 @@ public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long courseId;
+    @Column(nullable = false)
     private String courseName;
+    @Column(nullable = false)
     private String courseCode;
     private int semester;
+    @Column(nullable = false)
+    private int departmentId;
 }
